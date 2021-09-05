619806b7392bbf31a02a7a8b2db7bcaa5e75213e
jsleemaster <roeowo123@naver.com>
2021-09-05 13:58:32 +0900

    array2-함수

--- Array/Array.js
+++ Array/Array.js
@@ -24,3 +24,50 @@ for (let value of fruit) {
 fruit.forEach(value, index => {
     console.log(value), console.log(index) // 사과,바나나, 0, 1
 })
+
+//4. 추가 ,삭제, 복사
+
+//push
+fruit.push('복숭아', '감')
+
+//pop
+fruit.pop() // console.log(fruit) // '사과','바나나','복숭아'
+const pop = fruit.pop() // console.log(fruit) // '사과','바나나','복숭아'
+console.log(pop) // '사과','바나나'
+
+//unshift
+fruit.unshift('귤', '오렌지')
+console.log(fruit) // '귤','오렌지','사과','바나나','복숭아'
+
+fruit.shift('자두', '체리');
+console.log(fruit) // '오렌지','사과','바나나'
+
+//shift와 unshift는 굉장히 느림 사용 x
+
+// 원하는 위치를 지우는 법
+
+//splice
+fruit.splice(1, 1) // 시작할 위치, 몇개를 지울지
+fruit.splice(1, 0) // 원하는 위치에서 넣을 수도 있다. ex)fruit.splice(1,0,'사과')
+console.log(fruit) // '오렌지','바나나'
+fruit.splice(1, 1, '사과', '귤') // 시작할 위치, 몇개를 지울지 , 그 자리에 뭘 넣을지,뭘넣을지 추가2
+console.log(fruit) // '오렌지','사과','귤'
+
+//5. 병합
+const fruit2 = ['체리', '망고'];
+
+new fruit3 = fruit.concat(fruit2);
+console.log(fruit3); // '오렌지','사과','귤','체리','망고'
+
+//6. 찾기
+console.log(fruit3.indexOf('체리')) // 3
+console.log(fruit3.indexOf('감')) // -1
+
+console.log(fruit3.includes('체리')) // true
+console.log(fruit3.includes('감')) // false
+
+fruit3.push('체리')
+//동일한 값이 있다면 가장 앞에서부터 일치하는걸 찾아서 반환하기 떄문에 뒤에서부터도 찾을 수 있다.
+console.log(fruit3.lastIndexOf('체리')) // 5
+
+

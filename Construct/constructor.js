dea0664d8414db714f35b3806fb49c785bb40ffc
jsleemaster <roeowo123@naver.com>
2021-09-20 15:44:42 +0900

    생성자

---
+++ Construct/constructor.js
@@ -0,0 +1,10 @@
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+    this.sayName = function () {
+        console.log(this.name)
+    }
+}
+const User1 = new User('smlee', 22)
+let user1 = new User('smlee', 24)
+user1.sayName() // smlee
